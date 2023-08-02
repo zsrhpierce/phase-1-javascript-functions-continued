0a17e34ff1a687ddd3ae7e72ae825d3a6d75659c
Cathy Pierce <zsrhpierce@gmail.com>
2023-08-02 12:31:47 -0600

    more functions

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+   
+    return `This Saturday, I want to ${activity}!`;
+    
+}
+
+
+function mondayWork(work = 'go to the office'){
+    return `This Monday, I will ${work}.`;
+};
+
+function wrapAdjective(adj = "*") {
+    return function (message) {
+        return `You are ${adj}${message}${adj}!`;
+    };
+}
+
+const wrappedAdjective = wrapAdjective("!!!"); 
+console.log(wrappedAdjective("a wonderful person"));
